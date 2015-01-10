c8c6331933626a20ea6a8f357e51be24762a0075
luisarturogutierrez89 <luis.gutierrez@gruposellcom.com>
2015-01-10 15:21:45 -0600

    Initial Commit

---
+++ Aerolineas Ejecutivas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Aerolineas Ejecutivas
+//
+//  Created by Arturo Gutierrez Juarez on 10/01/15.
+//  Copyright (c) 2015 Arturo Gutierrez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Aerolineas Ejecutivas/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Aerolineas Ejecutivas
+//
+//  Created by Arturo Gutierrez Juarez on 10/01/15.
+//  Copyright (c) 2015 Arturo Gutierrez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
